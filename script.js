5e0aec816442437dcc6ea0bbaa776e21e0a8ada2
Dev Kaliper <gxlthz@gmail.com>
2023-04-15 13:53:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let scene = document.getElementById('scene');
+let parallaxInstanceScene = new Parallax(scene);
+let text = document.getElementById('text');
+let parallaxInstanceText = new Parallax(text);
+let imgCont = document.getElementById('imgcontainer');
+let parallaxInstanceimgCont = new Parallax(imgCont);
